923251deaa756289904535fd14b66c6c5f266de8
Piotr Pelan <piotr4pelan@gmail.com>
2023-03-28 19:51:07 +0200

    Handle cors

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -15,6 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:8080"));
 
 app.UseHttpsRedirection();
 
